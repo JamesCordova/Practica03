611d92319fb90244030ba0f3fab6088e9d343779
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-16 13:04:35 -0500

    metodo de insercion con el comportamiento de un entero

--- TADHeap.java
+++ TADHeap.java
@@ -4,7 +4,14 @@ public class TADHeap<T extends Comparable<T>>{
   private Arraylist<T> heap;
 
   public void insert(){
-
+    a[++n] = x;
+    T temp;
+    for(j=n; j>1 && a[j]>a[j/2]; j/=2) {
+      //intercambiamos con el padre
+      temp = a[j];
+      a[j] = a[j/2];
+      a[j/2] = temp;
+    }
   }
   public T remove(){
 
