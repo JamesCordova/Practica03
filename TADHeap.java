f6d0b0c89bc816694b383c74d93c41e52576dc6d
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-17 08:53:52 -0500

    Error solucionado de index fuera del rango corregido

--- TADHeap.java
+++ TADHeap.java
@@ -22,7 +22,7 @@ public class TADHeap<T extends Comparable<T>>{
                           // a[1] = a[n--];
     T ult = heap.get(heap.size() - 1); // tomamos el valor del ultimo item
     heap.set(0, ult); // reemplazamos el valor del primero con el ultimo item
-    heap.remove(heap.size()); // eliminamos el ultimo valor
+    heap.remove(heap.size() - 1); // eliminamos el ultimo valor
     int j = 1; // sera el padre por donde empezar
     while(2 * j < heap.size() - 1) { // mientras tenga algún hijo
       int k = 2 * j; // el hijo izquierdo
