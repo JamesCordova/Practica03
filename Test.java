ce4160e80172bf9cad7a4819fddb4e9b6a81d6e5
JamesCordova <jcordovas@unsa.edu.pe>
2023-06-17 08:06:11 -0500

    Pruebas con más elementos

--- Test.java
+++ Test.java
@@ -3,6 +3,10 @@ public class Test{
     TADHeap<Integer> n = new TADHeap<Integer>();
     n.insert(12);
     n.insert(2);
+    n.insert(42);
+    n.insert(22);
+    n.insert(19);
+    n.insert(1);
     n.insert(6);
     n.insert(10);
     System.out.println(n);
